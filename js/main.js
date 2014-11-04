4f42ea572504374409ccf25275d040dc380885c6
Alexander Osou <alex.osou@gmx.at>
2014-11-04 21:30:11 +0100

    dodecahedron from vertices and indices

--- js/main.js
+++ js/main.js
@@ -5,20 +5,42 @@ var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-Math.PHI = (1.0 + Math.sqrt(5)) / 2.0;
+// var parent = new Dodecahedron();
+// scene.add(parent);
 
-var parent = new Dodecahedron();
+var material = new THREE.MeshLambertMaterial({ 
+	// wireframe: true,
+	// side: THREE.DoubleSide,
+});
+
+var geometry = new THREE.Geometry();
+
+for (var i = 0; i < vertices.length; ++i) {
+	geometry.vertices.push(new THREE.Vector3(vertices[i][0], vertices[i][1], vertices[i][2]));
+}
+
+for (var i = 0; i < faces.length; ++i) {
+	var f = faces[i];
+	geometry.faces.push(new THREE.Face3(f[0], f[1], f[2]));
+	geometry.faces.push(new THREE.Face3(f[0], f[2], f[3]));
+	geometry.faces.push(new THREE.Face3(f[0], f[3], f[4]));
+}
+
+geometry.computeVertexNormals();
+
+var parent = new THREE.Mesh(geometry, material);
 scene.add(parent);
 
 var light = new THREE.DirectionalLight(0xffffff);
 light.position.set(0,1,1);
 
 var ambient = new THREE.AmbientLight(0x222222);
+// var ambient = new THREE.AmbientLight(0xffffff);
 
 scene.add(light);
 scene.add(ambient);
 
-camera.position.z = 7;
+camera.position.z = 5;
 
 var render = function () {
 	requestAnimationFrame(render);
